ba038b7d5d86655190aae6824cb367e23c6e01aa
hecjav1992 <easydata10@gmail.com>
2025-10-27 09:14:26 -0500

    vista de consulta 2

--- src/app/consultas/consultas.component.ts
+++ src/app/consultas/consultas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   standalone: false,
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './consultas.component.html',
   styleUrls: ['./consultas.component.css']
 })
-export class ConsultasComponent {
+export class ConsultasComponent implements OnInit {
 
+  usuarios = [
+    { id: '001',cedula:'1234-321-4332', nombre: 'Juan',apellido: 'Pérez' },
+    { id: '002', cedula:'7-708-781', nombre: 'María',apellido:'Gómez' },
+    { id: '003', cedula:'8-867-1844', nombre: 'Pedro',apellido:'Díaz' },
+  ];
+
+  filtroUsuario = '';
+  usuariosFiltrados = this.usuarios;
+
+  ngOnInit(): void {
+    this.usuariosFiltrados = this.usuarios;
+  }
+
+  filtrarUsuarios() {
+    const filtro = this.filtroUsuario.toLowerCase();
+    this.usuariosFiltrados = this.usuarios.filter(
+      (u) =>
+        u.id.toLowerCase().includes(filtro) ||
+        u.nombre.toLowerCase().includes(filtro)
+    );
+
+  }
 }
