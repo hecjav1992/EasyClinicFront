05267248c9904801ea1d5047abbe29c9d3a5e889
hecjav1992 <easydata10@gmail.com>
2025-10-31 21:35:10 -0500

    vista de atencion

--- src/app/consultas/consultas.component.ts
+++ src/app/consultas/consultas.component.ts
@@ -2,45 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ConsultaService } from '../../service/consulta.service';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   standalone: false,
   selector: 'app-consultas',
   templateUrl: './consultas.component.html',
   styleUrls: ['./consultas.component.css']
 })
-export class ConsultasComponent implements OnInit {
+export class ConsultasComponent  {
 
-
-  usuarios = [
-    { id: '001',cedula:'1234-321-4332', nombre: 'Juan',apellido: 'Pérez' },
-    { id: '002', cedula:'7-708-781', nombre: 'María',apellido:'Gómez' },
-    { id: '003', cedula:'8-867-1844', nombre: 'Pedro',apellido:'Díaz' },
-  ];
+  usuarios: any[]=[];
   constructor(private ConsultaService: ConsultaService) { }
 
   filtroUsuario = '';
   usuariosFiltrados = this.usuarios;
 
-  ngOnInit(): void {
-    this.usuariosFiltrados = this.usuarios;
-  }
-
-  carga() {
+ 
+  
+  filtrarUsuarios() {
     this.ConsultaService.consultapaciente().subscribe({
       next: res => {
-        console.log(res);
+        this.usuariosFiltrados = res.message;
       }
     });
-  }
- 
-
-  filtrarUsuarios() {
     const filtro = this.filtroUsuario.toLowerCase();
     this.usuariosFiltrados = this.usuarios.filter(
       (u) =>
         u.id.toLowerCase().includes(filtro) ||
         u.nombre.toLowerCase().includes(filtro)
     );
+   
 
   }
+  
 }
